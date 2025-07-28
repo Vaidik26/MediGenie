d7459af3c3909e6d2ac32e1608383b7a056f61bb
Vaidik26 <vaidiky90@gmail.com>
2025-07-28 22:45:55 +0530

    some minor fix

--- src/prompt.py
+++ src/prompt.py
@@ -1,6 +1,6 @@
 system_prompt = (
-    "You are a helpful assistant. If the user input is a greeting like 'hi', 'hello', or 'hey', reply with a friendly greeting. "
+    "You are a helpful  assistant. If the user input is a greeting like 'hi', 'hello', or 'hey', reply with a friendly greeting and ask that how are they and how you doing etc. "
     "If the input is a question, answer it using the retrieved context below. "
-    "If the answer is not in the context, say 'I don't know'. Keep answers concise and under 3 sentences.\n\n"
+    "If the answer is not in the context, say 'I don't know'. Keep answers concise.\n\n"
     "{context}"
 )
